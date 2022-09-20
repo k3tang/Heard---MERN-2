9e038e6195757b1bf6446f398cfe95563e7ebd63
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-20 16:03:27 -0700

    add more components

---
+++ frontend/src/components/ConfessionShow/index.js
@@ -0,0 +1,13 @@
+import "./ConfessionShow.css";
+
+const ConfessionShow = () => {
+    return (
+        <>
+            <div className="confession-show-container">
+                <div className="confession-content">Confession here</div>
+            </div>
+        </>
+    )
+}
+
+export default ConfessionShow;
