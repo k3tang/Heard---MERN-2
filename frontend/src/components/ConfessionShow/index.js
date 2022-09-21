97c15e2de52d236a5c77f37f1a7c98a095d2f610
“NAME” <EMAIL@EMAIL.COM>
2022-09-21 11:19:51 -0700

    confession show kinda works

--- frontend/src/components/ConfessionShow/index.js
+++ frontend/src/components/ConfessionShow/index.js
@@ -1,5 +1,5 @@
 import "./ConfessionShow.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchConfessions, getConfessions } from "../../store/confessions";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -7,6 +7,9 @@ import { useDispatch, useSelector } from "react-redux";
 const ConfessionShow = () => {
     const dispatch = useDispatch();
     const confessions = useSelector(getConfessions)
+    const [showConfession, setShowConfession] = useState(true)
+
+    // const showConfession = false
     
     useEffect(() => {
         dispatch(fetchConfessions())
@@ -19,9 +22,16 @@ const ConfessionShow = () => {
     console.log(random)
     console.log(posts[random])
 
+    const hideConfession = () =>{
+        document.getElementsByClassName('confession-content').style.display = 'none'
+    }
+    console.log(showConfession)
+
     useEffect(() => {
-        
-    })
+        setTimeout(function () {
+            setShowConfession(true);
+        }, 5000)
+    }, [])
     // setTimeout(confessionTimer() {
     //     $('#confession-content').fadeOut('fast');
     // }, 1000)
@@ -30,13 +40,17 @@ const ConfessionShow = () => {
     return (
         <>
             <div className="confession-show-container">
-                <div className="confession-content">Confession here
-                    <p>{randomConfession.mood}</p>
-                    <p>{randomConfession.body}</p>
-                </div>
+                {showConfession ? (
+                    <div className="confession-content" style={{display: showConfession ? 'block' : 'none'}}>Confession here
+                        <p>{randomConfession.mood}</p>
+                        <p>{randomConfession.body}</p>
+                    </div> ) : (
+                        <div />
+                    )}                
             </div>
         </>
     )
 }
 
 export default ConfessionShow;
+
