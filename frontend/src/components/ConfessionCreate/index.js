f0adc2febc57b93b9cb3d26f303a66fa1af125bc
“NAME” <EMAIL@EMAIL.COM>
2022-09-22 09:29:22 -0700

    create form styled

--- frontend/src/components/ConfessionCreate/index.js
+++ frontend/src/components/ConfessionCreate/index.js
@@ -55,14 +55,14 @@ function ConfessionCreate () {
 
     return (
         <>
-        <h2 class='confession-create-title'> you may confess my child </    h2>
+        {/* <h2 class='confession-create-title'> you may confess my child </    h2> */}
 
         <div class='confession-form-container'>
             <form class='confession-create-form' onSubmit={handleSubmit}>
                 <div class='mood-input-container'>
                     {/* <label> mood </label> */}
-                        <select name="mood" id="mood" value={mood} onChange={update('mood')}>
-                            {/* <option disabled> describe the vibe</option> */}
+                        <select className='confession-mood-dropdown'name="mood" id="mood" value={mood} onChange={update('mood')}>
+                            <option defaultValue value='invalid'> I'm feeling...</option>
                             <option value="angry" >Angry</option>
                             <option value="loved" >Loved</option>
                             <option value="anxious">Anxious</option>
@@ -72,13 +72,18 @@ function ConfessionCreate () {
                        
                 </div>
                 {/* <label> body </label> */}
+                <div className="confession-textarea-container">
                     <textarea 
                         type='text' 
+                        className="confession-text-area"
                         value={body} 
+                        placeholder="what's on your mind?"
                         onChange={update('body')} />
+                </div>
                 <input 
                     className="form-submit-button"
                     type='submit' 
+                     disabled={!mood || !body}
                     value='confess'/>
             </form>
         </div>
