2cea3c9e58d85882b771ec52fabd610bd5be0bef
Karen Siu <107089418+k3tang@users.noreply.github.com>
2022-09-22 11:43:36 -0700

    moved production code

--- app.js
+++ app.js
@@ -1,69 +1,103 @@
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const jwt = require('jsonwebtoken');
-const { secretOrKey } = require('./keys');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('./models/User');
+require('./models/Post');
+require('./models/Comment')
+// require('./config/passport');
 const passport = require('passport');
 
-passport.use(new LocalStrategy({
-    session: false,
-    usernameField: 'email',
-    passwordField: 'password',
-}, async function (email, password, done) {
-    const user = await User.findOne({ email });
-    if (user) {
-        bcrypt.compare(password, user.hashedPassword, (err, isMatch) => {
-            if (err || !isMatch) done(null, false);
-            else done(null, user);
-        });
-    } else
-        done(null, false);
-}));
+const debug = require('debug');
+const cors = require('cors');
+const csurf = require('csurf');
 
-exports.loginUser = async function (user) {
-    const userInfo = {
-        _id: user._id,
-        username: user.username,
-        email: user.email
-    };
-    const token = await jwt.sign(
-        userInfo, // payload
-        secretOrKey, // sign with secret key
-        // Tell the key to expire in one hour
-        { expiresIn: 3600 }
-    );
-    return {
-        user: userInfo,
-        token
-    };
-};
+// const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/api/users');
+const postsRouter = require('./routes/api/posts');
+const commentsRouter = require('./routes/api/comments');
 
-const options = {};
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = secretOrKey;
+const app = express();
 
-passport.use(new JwtStrategy(options, async (jwtPayload, done) => {
-    try {
-        const user = await User.findById(jwtPayload._id)
-        if (user) {
-            // return the user to the frontend
-            return done(null, user);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(passport.initialize());
+
+const { isProduction } = require('./config/keys');
+
+if (!isProduction) {
+    // enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000)
+    // (In production, React files will be served statically on the Express server)
+    app.use(cors());
+}
+
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
         }
-        // return false since there is no user
-        return done(null, false);
-    }
-    catch (err) {
-        done(err);
-    }
-}));
+    })
+);
 
-exports.requireUser = passport.authenticate('jwt', { session: false });
+const csrfRouter = require('./routes/api/csrf');
 
-exports.restoreUser = (req, res, next) => {
-    return passport.authenticate('jwt', { session: false }, function (err, user) {
-        if (user) req.user = user;
-        next();
-    })(req, res, next);
-};
+// app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
+app.use('/api/posts', postsRouter);
+app.use('/api/comments', commentsRouter);
+
+// Production, deploying to heroku
+if (isProduction) {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    app.get('/', (req, res) => {
+        res.cookie('CSRF-TOKEN', req.csrfToken());
+        res.sendFile(
+            path.resolve(__dirname, './frontend', 'build', 'index.html')
+        );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    app.use(express.static(path.resolve("./frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    app.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('CSRF-TOKEN', req.csrfToken());
+        res.sendFile(
+            path.resolve(__dirname, './frontend', 'build', 'index.html')
+        );
+    });
+}
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
+
+
+
+
+module.exports = app;
