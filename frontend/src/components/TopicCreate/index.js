2c60e6f308d1e8c570b06b2f47ebb2d6451cd80d
canjalal <vincent.shuali@gmail.com>
2022-10-03 19:12:01 -0700

    Add front end error handling for topic create and fall back backend errors

--- frontend/src/components/TopicCreate/index.js
+++ frontend/src/components/TopicCreate/index.js
@@ -11,10 +11,12 @@ import { clearConfessionErrors } from "../../store/confessions";
 const TopicCreate = () => {
     const dispatch = useDispatch();
     const [topicTitle, setTopicTitle] = useState("");
-    const [mood, setMood] = useState("loved");
+    const [mood, setMood] = useState("invalid");
     const errors = useSelector(state => state.errors);
      const sessionUser = useSelector(state => state.session.user);
      const history = useHistory();
+     const [isFormComplete, setIsFormComplete] = useState(false);
+     const [titleError, setTitleError] = useState("Make sure your topic title is at least 3 characters long!");
      const userId = sessionUser._id;
     //  const [error,setError] = useState(); 
 
@@ -29,9 +31,37 @@ const TopicCreate = () => {
         dispatch(getCurrentUser);
     }, []);
 
+    useEffect(() => {
+        document.querySelector(".topic-submit-button").disabled = !isFormComplete;
+        document.querySelector(".topic-submit-button").style.cursor = isFormComplete ? "pointer" : "not-allowed";
+    }, [isFormComplete])
+
     useEffect(() =>{
         dispatch(clearTopicErrors())
     }, [dispatch]);
+
+
+    const changeTitle = (e) => {
+        document.querySelector("#title-error").style.display = "block";
+        document.querySelector("#mood-error").style.color = "red";
+        setTopicTitle(e.target.value);
+        if(e.target.value.length < 3) {
+            setTitleError("Make sure your topic title is at least 3 characters long!");
+            setIsFormComplete(false);
+        } else if(e.target.value.length > 60) {
+            setTitleError("Make sure your topic title is 60 characters or less!");
+            setIsFormComplete(false);
+        } else {
+            setTitleError("");
+            setIsFormComplete(mood !== "invalid");
+        }
+    }
+
+    const changeMood = (e) => {
+        document.querySelector("#mood-error").style.color = "red";
+        setMood(e.target.value);
+        setIsFormComplete(e.target.value !== "invalid" && titleError === "");
+    }
 
 
     const handleSubmit = async (e) => {
@@ -64,7 +94,7 @@ const TopicCreate = () => {
             <div className="topic-create-container">
                 <div className="topic-form">
                     <form onSubmit={handleSubmit}>
-                            <select className="topic-mood-dropdown" value={mood} onChange={(e) => setMood(e.target.value)}>
+                            <select className="topic-mood-dropdown" value={mood} onChange={changeMood}>
                                 <option defaultValue value='invalid'> I'm feeling...</option>
                                 <option value="angry">Angry</option>
                                 <option value="loved">Loved</option>
@@ -72,9 +102,15 @@ const TopicCreate = () => {
                                 <option value="happy">Happy</option>
                                 <option value="sad">Sad</option>
                             </select>
+                            <div className="errors" id="mood-error">
+                            {mood === "invalid" ? "Please select a mood above" : ""}
+                          </div>
                              <textarea className="topic-form-text" rows="2" cols="50"
-                            value={topicTitle} placeholder="  Enter a title" onChange={(e) => setTopicTitle(e.target.value)} />
-                        <input className="topic-submit-button" type="submit" value="Create Topic"/>                       
+                            value={topicTitle} placeholder="  Enter a title" onChange={changeTitle} />
+                            <div className="errors" id="title-error">
+                            {titleError}
+                          </div>
+                        <input className="topic-submit-button" type="submit" value="Create Topic"  />                       
                     </form>
                     <div className="topic-errors">{errors?.topics}</div>
                 </div>
