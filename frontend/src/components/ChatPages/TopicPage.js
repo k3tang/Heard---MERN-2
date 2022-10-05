662721cf36b6a9b47026aff32749d1c1a85c88b0
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-10-05 12:24:01 -0700

    fixes routing error to random chat

--- frontend/src/components/ChatPages/TopicPage.js
+++ frontend/src/components/ChatPages/TopicPage.js
@@ -1,50 +1,71 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import ChatBox from "./ChatBox/ChatBox";
+import { useSelector, useDispatch } from "react-redux";
+import { getCurrentUser, _getCurrentUser } from "../../store/session";
+import MyTopicsDrawer from "../TopicIndex/MyTopicsDrawer";
+import { Spinner } from "@chakra-ui/react";
 
-import ChatBox from "./ChatBox/ChatBox"
-import { useSelector, useDispatch } from 'react-redux'
-import { getCurrentUser, _getCurrentUser } from '../../store/session'
-import MyTopicsDrawer from '../TopicIndex/MyTopicsDrawer'
+import FlagModal from "../FlagModal";
+import { getTopic } from "../../store/topics";
 
-
-import FlagModal from '../FlagModal'
-import { getTopic } from "../../store/topics"
-
-import { fetchTopic } from '../../store/topics'
-import "./chatpage.css"
-
+import { fetchTopic } from "../../store/topics";
+import "./chatpage.css";
 
 function TopicPage() {
- const {topicId} = useParams();
- const dispatch = useDispatch()
+  const { topicId } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const topic = useSelector(getTopic(topicId));
+  const [loading, setLoading] = useState(true);
 
-  const topic = useSelector(getTopic(topicId))
+  useEffect(() => {
+    if (!topic && !loading) {
+      history.push("/topic-index");
+    }
+  }, [topic, loading]);
 
+  setTimeout(() => {
+    setLoading(false);
+  }, 800);
 
-
-
-
-  const currentUser = useSelector(_getCurrentUser)
-  useEffect(()=>{
-    dispatch(getCurrentUser())
-    dispatch(fetchTopic(topicId))
- 
-  },[])
-
+  const currentUser = useSelector(_getCurrentUser);
+  useEffect(() => {
+    dispatch(getCurrentUser());
+    dispatch(fetchTopic(topicId));
+  }, []);
 
   return (
     <>
-     <MyTopicsDrawer />
-      <div className='chat-title-container'>
-        <h1 className='chat-title'>{topic?.title}</h1>
-        {topic?.flagged.isFlagged && <h2 className="flagged-text" style={{color: 'red'}}>THIS TOPIC HAS BEEN FLAGGED FOR REVIEW</h2>}
-      {topic && <FlagModal topic={topic}/>}
-      </div>
-      {currentUser && <ChatBox />}
-  </>
-  )
+      {loading ? (
+        <>
+          <MyTopicsDrawer />
+          <Spinner
+            className="spinner"
+            thickness="4px"
+            speed="0.65s"
+            size="lg"
+          />
+        </>
+      ) : (
+        <>
+          <MyTopicsDrawer />
+          <div className="chat-title-container">
+            <h1 className="chat-title">{topic?.title}</h1>
+            {topic?.flagged.isFlagged && (
+              <h2 className="flagged-text" style={{ color: "red" }}>
+                THIS TOPIC HAS BEEN FLAGGED FOR REVIEW
+              </h2>
+            )}
+            {topic && <FlagModal topic={topic} />}
+          </div>
+          <>{currentUser && <ChatBox />}</>
+        </>
+      )}
+    </>
+  );
 }
- 
-export default TopicPage;
 
+export default TopicPage;
