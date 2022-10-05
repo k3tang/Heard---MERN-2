9383f1d59eae7e261da2ac9c16631b0d77915d1e
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-10-05 11:44:19 -0700

    Remove dead code from TopicPage

--- frontend/src/components/ChatPages/TopicPage.js
+++ frontend/src/components/ChatPages/TopicPage.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Box } from "@chakra-ui/layout"
+
 import ChatBox from "./ChatBox/ChatBox"
 import { useSelector, useDispatch } from 'react-redux'
 import { getCurrentUser, _getCurrentUser } from '../../store/session'
 import MyTopicsDrawer from '../TopicIndex/MyTopicsDrawer'
-import { Button } from '@chakra-ui/react'
-import Modal2 from './modal2'
-import { fetchMessages, getAllMessages } from '../../store/messages'
+
 
 import FlagModal from '../FlagModal'
 import { getTopic } from "../../store/topics"
@@ -24,16 +22,14 @@ function TopicPage() {
   const topic = useSelector(getTopic(topicId))
 
 
-//  const [title, set Title] = useState("");
+
 
 
   const currentUser = useSelector(_getCurrentUser)
   useEffect(()=>{
     dispatch(getCurrentUser())
     dispatch(fetchTopic(topicId))
-    // .then(res => {
-    //   setTitle(res.title);
-    // });
+ 
   },[])
 
 
